868837399573ff4a20df8097743c1cc2bfbdb209
Ikundabayo Placide <ikundabayoplacide500@gmail.com>
2026-07-14 09:32:50 +0200

    modified codes

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const cors = require('cors');
 const app = express();
 
 // Middleware
-app.use(cors({ origin: 'http://localhost:8080', credentials: true }));
+app.use(cors({ origin: process.env.CORS_ORIGIN || 'http://localhost:8080', credentials: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
